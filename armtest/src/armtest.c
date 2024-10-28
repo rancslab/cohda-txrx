0303a4b102a721446388fe06ade7cb5664c61ecd
bailey <duser@MKx-SDK>
2024-10-28 00:29:14 +0000

    Hello world test for arm was added

---
+++ armtest/src/armtest.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : armtest.c
+ Author      : Bailey Bradley
+ Version     :
+ Copyright   : Nope
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hey"); /* prints Hey */
+	return EXIT_SUCCESS;
+}
